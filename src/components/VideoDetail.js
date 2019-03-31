63062df23d117e5f35a2f02d74f6f34929ad7595
Rodrigo Okuta <rodrigo.okuta@gmail.com>
2019-03-31 19:10:21 -0400

    created video detail and create a style for the page

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({ video }) => {
         <div>
             <div className="ui embed">
                 <iframe width="560" height="315" src={videoSrc}
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title={video.snippet.title} />
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="video player" />
             </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import VideoItem from './VideoItem';
+import React from "react";
+import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, onVideoSelect }) => {
-    const renderList = videos.map(video => {
-        return <VideoItem key={video.id.videoId || video.id.channelId} video={video} onVideoSelect={onVideoSelect} />
-    })
-
+  const renderList = videos.map(video => {
     return (
-        <div className="ui relaxed divided list">
-            {renderList}
-        </div>
-    )
-}
+      <VideoItem
+        key={video.id.videoId || video.id.channelId}
+        video={video}
+        onVideoSelect={onVideoSelect}
+      />
+    );
+  });
+
+  return <div className="ui relaxed divided list">{renderList}</div>;
+};
 
 VideoList.defaultProps = {
-    videos: [],
-}
+  videos: []
+};
 
-export default VideoList
+export default VideoList;
